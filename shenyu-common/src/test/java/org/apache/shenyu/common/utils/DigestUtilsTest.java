750557837cdecc62b270e0170731a80f94fa76a9
lianjunwei <lianjunwei@didiglobal.com>
2023-02-07 10:56:12 +0800

    版本升级到2.5.1

--- shenyu-common/src/test/java/org/apache/shenyu/common/utils/DigestUtilsTest.java
+++ shenyu-common/src/test/java/org/apache/shenyu/common/utils/DigestUtilsTest.java
@@ -29,6 +29,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DigestUtilsTest {
     @Test
     public void testSha512Hex() {
+        String sha512Hex = DigestUtils.sha512Hex("123456");
         assertThat(DigestUtils.sha512Hex("123456"),
                 is("ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413"));
     }
